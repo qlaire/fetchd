1f657d7d39bb1623ea7a66a51a76d0417c39ce92
Claire Dranginis <cdranginis@gmail.com>
2016-08-07 12:38:18 -0400

    added tennis ball background and changed sizing

--- browser/js/directives/ball.js
+++ browser/js/directives/ball.js
@@ -7,9 +7,8 @@ app.directive('ball', function() {
     link: function(scope, elem, attrs) {
       
       scope.$watch('pet.match', function(newVal, oldVal) {
-        let oldHeight = elem.css('height');
-        let newHeight = (newVal + 1) * 15;
-        console.log('the new height', newHeight);
+        let newHeight = Math.pow(newVal + 1, 2) * 30;
+        if (newHeight > 480) {newHeight = 480}
         if (newVal > 0 && newVal > oldVal) {
           elem.css('height', newHeight + 'px');
           elem.css('width', newHeight + 'px');
